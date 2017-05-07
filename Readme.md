a892eb0aad080cadab84993d39fd9c2fbdc1e38d
Damian Krzeminski <pirxpilot@code42day.com>
2017-05-06 20:19:36 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,34 @@
+[![NPM version][npm-image]][npm-url]
+[![Build Status][travis-image]][travis-url]
+[![Dependency Status][gemnasium-image]][gemnasium-url]
+
+# run-until
+
+ Run a sequence of async functions until one of them returns something.
+
+## Install
+
+```sh
+$ npm install --save run-until
+```
+
+## Usage
+
+```js
+var runUntil = require('run-until');
+
+runUntil('Rainbow');
+```
+
+## License
+
+MIT © [Damian Krzeminski](https://code42day.com)
+
+[npm-image]: https://img.shields.io/npm/v/run-until.svg
+[npm-url]: https://npmjs.org/package/run-until
+
+[travis-url]: https://travis-ci.org/pirxpilot/run-until
+[travis-image]: https://img.shields.io/travis/pirxpilot/run-until.svg
+
+[gemnasium-image]: https://img.shields.io/gemnasium/pirxpilot/run-until.svg
+[gemnasium-url]: https://gemnasium.com/pirxpilot/run-until
---
+++ test/run-until.js
@@ -0,0 +1,64 @@
+var assert = require('assert');
+var runUntil = require('../');
+
+describe('run-until', function () {
+
+  it('should run a single task', function (done) {
+
+    function task(params, fn) {
+      setTimeout(function() {
+        fn(null, params.v);
+      }, 1);
+    }
+
+    runUntil([ task ], { v: 5 }, function(err, v) {
+      assert.equal(v, 5);
+      done(err);
+    });
+  });
+
+
+  it('should runs all tasks until the first one', function (done) {
+    var seq = '';
+
+    function task(opts, params, fn) {
+      seq += opts.s;
+      assert(params.x, 5);
+      setTimeout(function() {
+        fn(opts.error, opts.v);
+      }, 1);
+    }
+
+    runUntil([
+        task.bind(null, { s: 'a' }),
+        task.bind(null, { s: 'b', error: 'not OK' }),
+        task.bind(null, { s: 'c', error: 'not OK' , v: 2 }),
+        task.bind(null, { s: 'd', v: 3 }),
+        task.bind(null, { s: 'e', v: 4 })
+      ], { x: 5 }, function(err, v) {
+        assert.equal(seq, 'abcd');
+        assert.equal(v, 3);
+        done(err);
+    });
+  });
+
+  it('should run all tasks if none returns any value', function (done) {
+    var seq = '';
+
+    function task(s, params, fn) {
+      seq += s;
+      setTimeout(fn, 1);
+    }
+
+    runUntil([
+        task.bind(null, 'a'),
+        task.bind(null, 'b'),
+        task.bind(null, 'c')
+      ], {}, function(err, v) {
+        assert.equal(seq, 'abc');
+        assert.equal(v, undefined);
+        done(err);
+    });
+  });
+
+});
